3aa88fac062336bb60bec9628ad3e7ca0f31c4ea
implodingduck <zsek320@gmail.com>
2024-06-18 10:44:21 -0500

    closing connection

--- appsocketapi/app/main.py
+++ appsocketapi/app/main.py
@@ -15,20 +15,17 @@ def read_root():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"You sent: {data}")
+    data = await websocket.receive_text()
+    await websocket.send_text(f"You sent: {data}")
 
 @app.websocket("/ws/hello")
 async def hello_websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Hello from sockets... You sent: {data}")
+    data = await websocket.receive_text()
+    await websocket.send_text(f"Hello from sockets... You sent: {data}")
 
 @app.websocket("/ws/world")
 async def world_websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"World from sockets... You sent: {data}")
+    data = await websocket.receive_text()
+    await websocket.send_text(f"World from sockets... You sent: {data}")
--- appsocketapi/client.py
+++ appsocketapi/client.py
@@ -3,6 +3,9 @@ from websockets.sync.client import connect
 
 URI="ws://localhost:5080/ws"
 
+
+#URI="wss://apim-quackersbankl6ht6vt8.azure-api.net/websocket"
+#URI="wss://socketapi.happygrass-f1771e78.eastus.azurecontainerapps.io/ws"
 def hello():
     with connect(URI) as websocket:
         websocket.send("Hello world!")
