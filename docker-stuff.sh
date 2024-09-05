1c8e7a649346ec0de299454928afad230386b0f9
implodingduck <zsek320@gmail.com>
2024-09-05 12:34:56 -0500

    updating requirements.txt

--- docker-stuff.sh
+++ docker-stuff.sh
@@ -2,11 +2,16 @@ cd appbasicapi
 docker build -t appbasicapi .
 cd ../apptimerapi
 docker build -t apptimerapi .
+cd ../apppubsub
+docker build -t apppubsub .
 
 docker stop appbasicapi
 docker rm appbasicapi
 docker stop apptimerapi
 docker rm apptimerapi
+docker stop apppubsub
+docker rm apppubsub
 
 docker run -d -p 5080:80 --name appbasicapi appbasicapi
 docker run -d -p 5088:80 --name apptimerapi apptimerapi
+docker run -d -p 5089:80 --name apppubsub apppubsub
