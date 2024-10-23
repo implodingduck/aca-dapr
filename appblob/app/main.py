4d5e6b452bd608703695434e3a7f90e03442096b
implodingduck <zsek320@gmail.com>
2024-10-22 19:27:17 -0500

    adding dapr binding items

--- appblob/app/main.py
+++ appblob/app/main.py
@@ -1,9 +1,10 @@
 from typing import Union
 
 from fastapi import FastAPI, Request
-
+from dapr.clients import DaprClient
 import json
-
+import uuid
+import datetime
 app = FastAPI()
 
 
@@ -15,3 +16,15 @@ def read_root():
 @app.get("/me")
 def me(request: Request):
     return {"headers": request.headers}
+
+@app.get("/create")
+def output_binding_create():
+    with DaprClient() as d:
+        resp = d.invoke_binding('outputbinding', 'create', data=f"{datetime.datetime.now()} - {uuid.uuid4()}\n", metadata={ "blobName": "myblob.txt"})
+    return {"resp": resp.text()}
+
+@app.get("/read")
+def output_binding_read():
+    with DaprClient() as d:
+        resp = d.invoke_binding('outputbinding', 'get', metadata={ "blobName": "myblob.txt", "includeMetadata": "true"})
+    return {"resp": resp.text()}
