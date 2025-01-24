51f9fcac829461a2c85f13f9edfeefec9bed8a06
implodingduck <zsek320@gmail.com>
2025-01-24 10:06:23 -0600

    adding an async timeout option

--- apptimerapi/app/main.py
+++ apptimerapi/app/main.py
@@ -19,3 +19,14 @@ def timeout(duration: int):
     time.sleep(duration)
     end = datetime.datetime.now()
     return {"duration": duration, "starttime": start, "endtime": end}
+
+async def sleepasync(duration: int):
+    time.sleep(duration)
+    return duration
+
+@app.get("/asynctimeout/{duration}")
+async def timeoutasync(duration: int):
+    start = datetime.datetime.now()
+    await sleepasync(duration)
+    end = datetime.datetime.now()
+    return {"duration": duration, "starttime": start, "endtime": end}
